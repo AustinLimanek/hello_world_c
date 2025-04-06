d72fae0aa2162a65b65b02d39e13c484b01d986f
Austin Limanek <austin.limanek@gmail.com>
2025-04-06 15:58:23 -0700

    Initial Commit

---
+++ hello_world/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hello_world
+//
+//  Created by Austin Limanek on 4/6/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
