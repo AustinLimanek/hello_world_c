e78360321f03f36813603d293c532fdfbda791fa
Austin Limanek <austin.limanek@gmail.com>
2025-04-07 11:55:02 -0700

    added some code to explore the different types and declaring functions.

--- hello_world/main.c
+++ hello_world/main.c
@@ -6,9 +6,38 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
+// int main(int argc, const char * argv[]) {
+void addOne (int *p){
+    *p = *p + 1;
+}
+
+int main(void) {
+
     // insert code here...
-    printf("Hello, World!\n");
+    // printf("Hello, World!\n");
+    int angle_degree;
+    double angle_radian, pi, value;
+    
+    printf("\nCompute a table of the sine function\n\n");
+    pi = M_PI;
+    printf("PI = %f \n\n", pi);
+    printf("Angle\tSine \n");
+    angle_degree = 0;
+    while(angle_degree <= 360){
+        angle_radian = angle_degree * pi / 180.0;
+        value = sin(angle_radian);
+        printf("%3d\t%f \n", angle_degree, value);
+        angle_degree += 10;
+    }
+    
+    int y = 3;
+    printf("%i\n", y);
+    addOne(&y);
+    printf("%i\n", y);
+    
+    int x;
+    printf("%zu\n", sizeof(x));
     return 0;
 }
